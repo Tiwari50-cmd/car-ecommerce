4864a3d285a5457542f1603ffe4a35d768480fc1
Tiwari50-cmd <156881991+Tiwari50-cmd@users.noreply.github.com>
2024-01-18 07:55:36 +0545

    Initial commit

---
+++ src/pages/CartPage.js
@@ -0,0 +1,5 @@
+import Cart from "../features/cart/carts";
+function CartPage() {
+  return <Cart></Cart>;
+}
+export default CartPage;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import NavBar from "../features/navbar/Navbar";
+import ProductList from "../features/product/components/ProductList";
+function Home() {
+  return (
+    <div>
+      <NavBar>
+        <ProductList></ProductList>
+      </NavBar>
+    </div>
+  );
+}
+export default Home;
